121cc8fd9a09d113d02f3c047db6dcbfe0831d7c
Pinal Parmar <70330704+Pinal2911@users.noreply.github.com>
2022-05-01 13:46:10 -0400

    Add files via upload

---
+++ START36D/chefandchapter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int c,u,ch;
+	    cin>>c>>u>>ch;
+	    cout<<c*u*ch<<endl;
+	}
+	return 0;
+}
